a0f573d05f90aa42374f9f2b5d1f40d7ff33d729
Abergster <47314911+Abergster@users.noreply.github.com>
2019-03-07 16:09:00 +0100

    Pie chart over parkering i vejle done + added til navigation

--- script/Parkeringspladser.js
+++ script/Parkeringspladser.js
@@ -43,7 +43,7 @@ function hentet(result) {
         }
         fundet=false;
     }
-
+/*
  var gadehtml ="";
 
     for(i=0;i<gader.length;i++)
@@ -53,39 +53,40 @@ function hentet(result) {
 
  var x = document.getElementById("myTable");
 
- x.innerHTML= gadehtml;
+ x.innerHTML= gadehtml;*/
   piechart();
 }
+
+var colorarray =['#FF6633', '#FFB399', '#FF33FF', '#FFFF99', '#00B3E6', 
+                '#E6B333', '#3366E6', '#999966', '#99FF99', '#B34D4D',
+                '#80B300', '#809900', '#E6B3B3', '#6680B3', '#66991A', 
+                '#FF99E6', '#CCFF1A', '#FF1A66', '#E6331A', '#33FFCC',
+                '#66994D', '#B366CC', '#4D8000', '#B33300', '#CC80CC', 
+                '#66664D', '#991AFF', '#E666FF', '#4DB3FF', '#1AB399',
+                '#E666B3', '#33991A', '#CC9999', '#B3B31A', '#00E680', 
+                '#4D8066', '#809980', '#E6FF80', '#1AFF33', '#999933',
+                '#FF3380', '#CCCC00', '#66E64D', '#4D80CC', '#9900B3', 
+                '#E64D66', '#4DB380', '#FF4D4D', '#99E6E6', '#6666FF']
 function piechart()
 {
     var ctx = document.getElementById("myChart");
-
-    var myPieChart = new Chart(ctx,{
+   
+    var myPieChart = new Chart(ctx, {
         type: 'pie',
         data: {
-            labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
-            datasets: [{
-                label: gader,
-                data: antal,
-                backgroundColor: [
-                    'rgba(255, 99, 132, 0.2)',
-                    'rgba(54, 162, 235, 0.2)',
-                    'rgba(255, 206, 86, 0.2)',
-                    'rgba(75, 192, 192, 0.2)',
-                    'rgba(153, 102, 255, 0.2)',
-                    'rgba(255, 159, 64, 0.2)'
-                ],
-                borderColor: [
-                    'rgba(255,99,132,1)',
-                    'rgba(54, 162, 235, 1)',
-                    'rgba(255, 206, 86, 1)',
-                    'rgba(75, 192, 192, 1)',
-                    'rgba(153, 102, 255, 1)',
-                    'rgba(255, 159, 64, 1)'
-                ],
-                borderWidth: 1
-            }]
+          labels: gader,
+          datasets: [{
+            label: "Antal Parkeringer",
+            backgroundColor: colorarray,
+            data: antal
+          }]
         },
-        options: options
+        options: {
+          title: {
+            display: true,
+            text: ' Parkeringspladser (kommunal ejede og offentlig tilgængelige) '
+          }
+        }
     });
+    
 }
