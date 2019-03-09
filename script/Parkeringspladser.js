b5240477867c349cce5446d55dfec6a9960803be
Abergster <47314911+Abergster@users.noreply.github.com>
2019-03-09 14:42:59 +0100

    Update Parkeringspladser.js

--- script/Parkeringspladser.js
+++ script/Parkeringspladser.js
@@ -1,12 +1,13 @@
 function getChartData() {
     $.ajax({
-        url: "http://kortservice.vejle.dk/gis/rest/services/OPENDATA/Vejle/MapServer/3/query?where=OBJECTID%3C%3E0&text=&objectIds=&time=&geometry=&geometryType=esriGeometryEnvelope&inSR=&spatialRel=esriSpatialRelIntersects&relationParam=&outFields=*&returnGeometry=true&maxAllowableOffset=&geometryPrecision=&outSR=&returnIdsOnly=false&returnCountOnly=false&orderByFields=&groupByFieldsForStatistics=&outStatistics=&returnZ=false&returnM=false&gdbVersion=&returnDistinctValues=false&f=pjson",
+        type: 'get',
+        url: 'http://kortservice.vejle.dk/gis/rest/services/OPENDATA/Vejle/MapServer/3/query?where=OBJECTID%3C%3E0&text=&objectIds=&time=&geometry=&geometryType=esriGeometryEnvelope&inSR=&spatialRel=esriSpatialRelIntersects&relationParam=&outFields=*&returnGeometry=true&maxAllowableOffset=&geometryPrecision=&outSR=&returnIdsOnly=false&returnCountOnly=false&orderByFields=&groupByFieldsForStatistics=&outStatistics=&returnZ=false&returnM=false&gdbVersion=&returnDistinctValues=false&f=pjson',
         success: function (result) {       
         
             hentet(result);
         },
         error: function (err) {
-            alert("fejl");
+            alert("der skete en fejl med hentning af dataen");
         }        
     });
 }
